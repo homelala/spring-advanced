4035aab4201620bcec6eb54e3361f3f85865d443
dale <homelala@naver.com>
2024-05-08 17:22:37 +0900

    로그 추적기 V2 생성

--- spring-advanced/src/main/java/spring/springadvanced/trace/TraceId.java
+++ spring-advanced/src/main/java/spring/springadvanced/trace/TraceId.java
@@ -7,8 +7,8 @@ import java.util.UUID;
 @Getter
 public class TraceId {
 
-    private String id;
-    private int level;
+    private final String id;
+    private final int level;
 
     public TraceId() {
         this.id = createId();
@@ -24,12 +24,12 @@ public class TraceId {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    public TraceId createNextId(String id, int level){
-        return new TraceId(id, level + 1);
+    public TraceId createNextId(){
+        return new TraceId(this.id, this.level + 1);
     }
 
-    public TraceId createPreviousId(String id, int level){
-        return new TraceId(id, level - 1);
+    public TraceId createPreviousId(){
+        return new TraceId(this.id, this.level - 1);
     }
 
     public boolean isFirstLevel(){
---
+++ spring-advanced/src/test/java/spring/springadvanced/trace/HelloTraceV2Test.java
@@ -0,0 +1,25 @@
+package spring.springadvanced.trace;
+
+import org.junit.jupiter.api.Test;
+import spring.springadvanced.hellotrace.HelloTraceV2;
+
+public class HelloTraceV2Test {
+    @Test
+    void begin_end(){
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status1);
+        trace.end(status2);
+
+    }
+
+    @Test
+    void begin_exception(){
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status1, new IllegalStateException());
+        trace.exception(status2, new IllegalStateException());
+    }
+}
