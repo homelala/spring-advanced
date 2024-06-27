ae320923a7e604118c23ba8a5e8aa65dcba1dda0
dale <homelala@naver.com>
2024-06-27 17:55:27 +0900

    어드바이저 적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.interface_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamic_proxy.DynamicProxyBasicHandler;
 import hello.proxy.config.v2_dynamic_proxy.DynamicProxyFilterHandler;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +20,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicHandler.class)
-@Import(DynamicProxyFilterHandler.class)
+//@Import(DynamicProxyFilterHandler.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
