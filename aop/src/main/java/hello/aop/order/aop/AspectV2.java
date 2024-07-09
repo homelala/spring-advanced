8cc48ec3ab0d72643d6d0201561f5929bb16a9c3
dale <homelala@naver.com>
2024-07-09 11:29:35 +0900

    aop 구현 - aspect, pointcut 추가

---
+++ aop/src/main/java/hello/aop/order/aop/AspectV2.java
@@ -0,0 +1,21 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder() {} // 포인트컷 signature
+
+    @Around("allOrder()") // 포인트 컷
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable { // 어드바이스
+        log.info("log -> {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
