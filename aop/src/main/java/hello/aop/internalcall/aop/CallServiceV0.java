d865f6aec6a3f228c0ae6c546371e59aed53ed89
dale <homelala@naver.com>
2024-07-12 10:57:35 +0900

    프록시 내부 호출 - 문제

---
+++ aop/src/main/java/hello/aop/internalcall/aop/CallServiceV0.java
@@ -0,0 +1,18 @@
+package hello.aop.internalcall.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV0 {
+    public void external() {
+        log.info("external");
+        internal();
+    }
+
+    public void internal() {
+        log.info("internal");
+    }
+
+}
